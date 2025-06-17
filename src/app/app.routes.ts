f617f91bedc6ec15a7dee62fa2226b57b031971c
erickha16 <errickha@gmail.com>
2025-06-17 10:53:39 -0600

    Initial structure created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+        path: 'home',
+        component: Home // Aquí se especifica el componente que se mostrará cuando la ruta sea 'home'
+    },
+    {
+        path:'', //Ruta raiz
+        redirectTo: 'home', //Redirigir a la ruta home
+        pathMatch: 'full' //Hacer que la ruta sea exacta
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavMenu } from "./layout/nav-menu/nav-menu";
+import { Footer } from "./layout/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavMenu, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected title = 'CoreHealth_Front';
