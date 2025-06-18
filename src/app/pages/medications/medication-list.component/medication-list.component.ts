80b000fff5c0ea69d8856f2ce9c56cb4e99d5863
ErickHa16 <errickha@gmail.com>
2025-06-18 12:26:16 -0600

    MedicationList TS GET implemented

--- src/app/pages/medications/medication-list.component/medication-list.component.ts
+++ src/app/pages/medications/medication-list.component/medication-list.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MedicationService } from '../../../services/medication.service';
+import { Medication } from '../../../interfaces/Medication.model';
 
 @Component({
   selector: 'app-medication-list.component',
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './medication-list.component.html',
   styleUrl: './medication-list.component.css'
 })
-export class MedicationListComponent {
+export class MedicationListComponent implements OnInit {
+  medicationService = inject(MedicationService) //Iyeción del servicio
+  medications: Medication[] = []; //Lista a mostrar
+  filterMedications: Medication[] = []; //Lista filtrada
+  searchTerm: string = ''; // Para la bárra de busqueda
 
-}
+  ngOnInit(): void {
+      this.lodadData();
+  }
+
+  lodadData(){
+    this.medicationService.getList().subscribe(data=>{
+      this.medications = data;
+      this.filterMedications = data;
+    })
+  }
+
+  filter(){
+    this.filterMedications = this.medications.filter(m=> m.name.toLowerCase().includes(this.searchTerm.toLowerCase()));
+  }
+  
+} 
