304cba61b3eb69be505d539685ff32ab55a0b3e2
ErickHa16 <errickha@gmail.com>
2025-06-18 22:40:51 -0600

    Patient modal created

---
+++ src/app/components/patient-detail-modal/patient-detail-modal.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Patient } from '../../interfaces/Patient.model';
+import { PatientService } from '../../services/patient.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-patient-detail-modal',
+  imports: [CommonModule],
+  templateUrl: './patient-detail-modal.html',
+  styleUrl: './patient-detail-modal.css'
+})
+export class PatientDetailModal {
+  @Input() patient!: Patient;
+
+  private patientService = inject(PatientService);
+  private router = inject(Router);
+  @Output() cerrar = new EventEmitter<void>(); 
+
+  
+  close(){
+    this.cerrar.emit(); 
+  }
+
+   deletePatient() {
+    if(confirm(`¿Eliminar definitivamente a ${this.patient.name}?`)) {
+      this.patientService.delete(this.patient.id).subscribe({
+        next: () => {
+          this.cerrar.emit(); 
+          this.router.navigate(['/patients']);
+        },
+        error: (err) => console.error('Error al eliminar', err)
+      });
+    }
+  }
+}
--- src/app/pages/patients/patients-list.component/patients-list.component.ts
+++ src/app/pages/patients/patients-list.component/patients-list.component.ts
@@ -5,17 +5,23 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Patient } from '../../../interfaces/Patient.model';
 import { PatientService } from '../../../services/patient.service';
 import { DatePipe } from '@angular/common';
+import { PatientDetailModal } from '../../../components/patient-detail-modal/patient-detail-modal';
 
 @Component({
   selector: 'app-patients-list.component',
-  imports: [RouterLink, MatSlideToggleModule, FormsModule, ReactiveFormsModule, DatePipe],
+  imports: [RouterLink, MatSlideToggleModule, FormsModule, ReactiveFormsModule, DatePipe, PatientDetailModal],
   templateUrl: './patients-list.component.html',
   styleUrl: './patients-list.component.css'
 })
 export class PatientsListComponent implements OnInit {
-  patientService = inject(PatientService);
+  private patientService = inject(PatientService);
 
   patients:Patient[] = []
+
+  //Modal
+  selectedPatient!:Patient;
+  showDetailModal: boolean = false;
+
 
   ngOnInit(): void {
       this.loadData();
@@ -28,12 +34,17 @@ export class PatientsListComponent implements OnInit {
   }
 
   deletePatient(id: number) {
-  if(confirm('¿Estás seguro de eliminar este paciente?')) {
-    this.patientService.delete(id).subscribe({
-      next: () => this.loadData(),
-      error: (err) => console.error('Error al eliminar', err)
-    });
+    if(confirm('¿Estás seguro de eliminar este paciente?')) {
+      this.patientService.delete(id).subscribe({
+        next: () => this.loadData(),
+        error: (err) => console.error('Error al eliminar', err)
+      });
+    }
   }
+
+  openDetails(patient: Patient) {
+      this.selectedPatient = patient;
+      this.showDetailModal = true;
+    }
 }
 
-}
